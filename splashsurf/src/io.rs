be12020f280c29e1cd8606fe38f0f95593f41c9f
Fabian Loeschner <loeschner@cs.rwth-aachen.de>
2020-12-01 11:13:39 +0100

    Handle some panics with errors instead

--- splashsurf/src/io.rs
+++ splashsurf/src/io.rs
@@ -111,39 +111,47 @@ pub fn particles_from_xyz<R: Real, P: AsRef<Path>>(
     Ok(particles)
 }
 
-pub fn particles_from_ply<R:Real, P:AsRef<Path>>(
-    ply_file: P
+pub fn particles_from_ply<R: Real, P: AsRef<Path>>(
+    ply_file: P,
 ) -> Result<Vec<Vector3<R>>, anyhow::Error> {
     let mut ply_file = std::fs::File::open(ply_file).unwrap();
     let parser = ply::parser::Parser::<ply::ply::DefaultElement>::new();
 
-    let ply = parser.read_ply(&mut ply_file);
+    let ply = parser
+        .read_ply(&mut ply_file)
+        .context("Failed to read PLY file")?;
+    let elements = ply
+        .payload
+        .get("vertex")
+        .ok_or(anyhow!("PLY file is missing a 'vertex' element"))?;
 
-    assert!(ply.is_ok());
-    let ply = ply.unwrap();
-
-    let elements = ply.payload.get("vertex").unwrap();
-
-    let points: Vec<Vector3<R>> = elements
+    let particles = elements
         .into_iter()
-        .map(|e| (e.get("x").unwrap(), e.get("y").unwrap(), e.get("z").unwrap()))
-        .map(|point| {
-            let vector: Vector3<R> = match point {
-                (Property::Float(x), Property::Float(y), Property::Float(z)) => 
-                    Vector3::new(
-                        R::from_f32(*x).unwrap(), 
-                        R::from_f32(*y).unwrap(), 
-                        R::from_f32(*z).unwrap()),
-                _ => panic!("Couldnt load point from ply file")
-                // _ => Vector3::new(R::from_f32(0.).unwrap(), R::from_f32(0.).unwrap(), R::from_f32(0.).unwrap())
+        .map(|e| {
+            let vertex = (
+                e.get("x").unwrap(),
+                e.get("y").unwrap(),
+                e.get("z").unwrap(),
+            );
 
+            let v = match vertex {
+                (Property::Float(x), Property::Float(y), Property::Float(z)) => Vector3::new(
+                    R::from_f32(*x).unwrap(),
+                    R::from_f32(*y).unwrap(),
+                    R::from_f32(*z).unwrap(),
+                ),
+                _ => {
+                    return Err(anyhow!(
+                        "Vertex properties have wrong PLY data type (expected float)"
+                    ))
+                }
             };
-            vector
-    
+
+            Ok(v)
         })
-        .collect();
-    
-    Ok(points)
+        .collect::<Result<Vec<_>, anyhow::Error>>()?;
+
+    Ok(particles)
 }
 
 #[allow(dead_code)]
