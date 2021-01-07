e15fecd1339b67b6a75a80ff74847553e6f816e0
Fabian Löschner <fabian.loeschner@rwth-aachen.de>
2021-01-07 14:18:07 +0100

    Remove old benchmark

--- splashsurf_lib/benches/benches/bench_octree.rs
+++ splashsurf_lib/benches/benches/bench_octree.rs
@@ -1,4 +1,4 @@
-use criterion::{black_box, criterion_group, BatchSize, Criterion};
+use criterion::{criterion_group, BatchSize, Criterion};
 use nalgebra::Vector3;
 use splashsurf_lib::octree::Octree;
 use splashsurf_lib::{grid_for_reconstruction, UniformGrid};
@@ -9,7 +9,7 @@ use bitflags::_core::time::Duration;
 pub fn subdivide_recursively_benchmark(c: &mut Criterion) {
     let particle_positions: &Vec<Vector3<f64>> =
         &particles_from_xyz("../data/canyon_13353401_particles.xyz").unwrap();
-    let particles_per_cell = 104323;
+    let particles_per_cell = 20000;
 
     let particle_radius = 0.011;
     let kernel_radius = 4.0 * particle_radius;
@@ -26,7 +26,7 @@ pub fn subdivide_recursively_benchmark(c: &mut Criterion) {
     let mut group = c.benchmark_group("Octree subdivision");
     group.sample_size(50);
     group.warm_up_time(Duration::from_secs(5));
-    group.measurement_time(Duration::from_secs(25));
+    group.measurement_time(Duration::from_secs(30));
 
     let get_tree = || Octree::new(&grid, particle_positions.len());
 
@@ -39,20 +39,6 @@ pub fn subdivide_recursively_benchmark(c: &mut Criterion) {
                     particle_positions.as_slice(),
                     particles_per_cell,
                     kernel_radius,
-                )
-            },
-            BatchSize::LargeInput,
-        )
-    });
-
-    group.bench_function("subdivide_recursively_par", move |b| {
-        b.iter_batched(
-            get_tree,
-            |mut tree| {
-                tree.subdivide_recursively_par(
-                    grid,
-                    particle_positions.as_slice(),
-                    particles_per_cell,
                 )
             },
             BatchSize::LargeInput,
