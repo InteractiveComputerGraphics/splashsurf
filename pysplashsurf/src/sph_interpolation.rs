55d51ad48f0530f2173c290cfd6e0665657c2a7e
Felix Kern <felixkern04@gmail.com>
2025-03-31 15:27:59 +0200

    SphInterpolator methods

--- pysplashsurf/src/sph_interpolation.rs
+++ pysplashsurf/src/sph_interpolation.rs
@@ -59,7 +59,6 @@ macro_rules! create_sph_interpolator_interface {
 
                 let normals_vec = self.inner.interpolate_normals(interpolation_points);
                 let normals_vec = bytemuck::allocation::cast_vec::<Unit<Vector3<$type>>, $type>(normals_vec);
-                //let normals_vec: Vec<$type> = bytemuck::cas
 
                 let normals: &[$type] = normals_vec.as_slice();
                 let normals: ArrayView2<$type> =
@@ -67,6 +66,33 @@ macro_rules! create_sph_interpolator_interface {
                         .unwrap();
                 
                 normals.to_pyarray(py)
+            }
+
+            /// Interpolates a vectorial per particle quantity to the given points, panics if the there are less per-particles values than particles
+            fn interpolate_vector_quantity<'py>(
+                &self, 
+                py: Python<'py>,
+                particle_quantity: &Bound<'py, PyArray2<$type>>, 
+                interpolation_points: &Bound<'py, PyArray2<$type>>, 
+                first_order_correction: bool
+            ) -> Bound<'py, PyArray2<$type>> {
+                let interpolation_points: PyReadonlyArray2<$type> = interpolation_points.extract().unwrap();
+                let interpolation_points = interpolation_points.as_slice().unwrap();
+                let interpolation_points: &[Vector3<$type>] = bytemuck::cast_slice(interpolation_points);
+
+                let particle_quantity: PyReadonlyArray2<$type> = particle_quantity.extract().unwrap();
+                let particle_quantity = particle_quantity.as_slice().unwrap();
+                let particle_quantity: &[Vector3<$type>] = bytemuck::cast_slice(particle_quantity);
+
+                let res_vec = self.inner.interpolate_vector_quantity(particle_quantity, interpolation_points, first_order_correction);
+                let res_vec = bytemuck::allocation::cast_vec::<Vector3<$type>, $type>(res_vec);
+
+                let res: &[$type] = res_vec.as_slice();
+                let res: ArrayView2<$type> =
+                    ArrayView::from_shape((res.len() / 3, 3), res)
+                        .unwrap();
+                
+                res.to_pyarray(py)
             }
         }
     };
