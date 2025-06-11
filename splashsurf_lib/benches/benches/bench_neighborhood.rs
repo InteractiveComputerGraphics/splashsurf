2f46e5b9cc632f2ae54022a287d60ed9bb4e2582
Fabian Löschner <fabian.loeschner@rwth-aachen.de>
2025-06-11 22:01:15 +0200

    Add benchmark for flat neighborhood search

--- splashsurf_lib/benches/benches/bench_neighborhood.rs
+++ splashsurf_lib/benches/benches/bench_neighborhood.rs
@@ -1,6 +1,7 @@
 use criterion::{Criterion, criterion_group};
 use nalgebra::Vector3;
 use splashsurf_lib::io;
+use splashsurf_lib::neighborhood_search::FlatNeighborhoodList;
 use splashsurf_lib::{Aabb3d, neighborhood_search};
 use std::time::Duration;
 
@@ -10,7 +11,8 @@ static COMPACT_SUPPORT_RADIUS: f64 = 4.0 * PARTICLE_RADIUS;
 //static NUM_PARTICLES: Option<usize> = Some(800);
 static NUM_PARTICLES: Option<usize> = None;
 
-static PARTICLE_FILE: &str = "../data/bunny_frame_14_7705_particles.vtk";
+//static PARTICLE_FILE: &str = "../data/bunny_frame_14_7705_particles.vtk";
+static PARTICLE_FILE: &str = "../data/dam_break_frame_23_24389_particles.bgeo";
 
 fn particle_subset(particle_positions: &[Vector3<f32>]) -> &[Vector3<f32>] {
     if let Some(n_particles) = NUM_PARTICLES {
@@ -21,8 +23,7 @@ fn particle_subset(particle_positions: &[Vector3<f32>]) -> &[Vector3<f32>] {
 }
 
 pub fn neighborhood_search_naive(c: &mut Criterion) {
-    let particle_positions: &Vec<Vector3<f32>> =
-        &io::vtk_format::particles_from_vtk(PARTICLE_FILE).unwrap();
+    let particle_positions: &Vec<Vector3<f32>> = &io::particles_from_file(PARTICLE_FILE).unwrap();
     let particle_positions = particle_subset(particle_positions.as_slice());
 
     let mut group = c.benchmark_group("neighborhood_search");
@@ -45,9 +46,8 @@ pub fn neighborhood_search_naive(c: &mut Criterion) {
     group.finish();
 }
 
-pub fn neighborhood_search_spatial_hashing(c: &mut Criterion) {
-    let particle_positions: &Vec<Vector3<f32>> =
-        &io::vtk_format::particles_from_vtk(PARTICLE_FILE).unwrap();
+pub fn neighborhood_search_spatial_hashing_seq(c: &mut Criterion) {
+    let particle_positions: &Vec<Vector3<f32>> = &io::particles_from_file(PARTICLE_FILE).unwrap();
     let particle_positions = particle_subset(particle_positions.as_slice());
 
     let mut domain = Aabb3d::from_points(particle_positions);
@@ -59,7 +59,7 @@ pub fn neighborhood_search_spatial_hashing(c: &mut Criterion) {
     group.measurement_time(Duration::from_secs(10));
 
     let mut neighborhood_lists = Vec::with_capacity(particle_positions.len());
-    group.bench_function("neighborhood_search_spatial_hashing", move |b| {
+    group.bench_function("neighborhood_search_spatial_hashing_seq", move |b| {
         b.iter(|| {
             neighborhood_lists.clear();
             neighborhood_search::neighborhood_search_spatial_hashing::<i32, f32>(
@@ -75,8 +75,7 @@ pub fn neighborhood_search_spatial_hashing(c: &mut Criterion) {
 }
 
 pub fn neighborhood_search_spatial_hashing_parallel(c: &mut Criterion) {
-    let particle_positions: &Vec<Vector3<f32>> =
-        &io::vtk_format::particles_from_vtk(PARTICLE_FILE).unwrap();
+    let particle_positions: &Vec<Vector3<f32>> = &io::particles_from_file(PARTICLE_FILE).unwrap();
     let particle_positions = particle_subset(particle_positions.as_slice());
 
     let mut domain = Aabb3d::from_points(particle_positions);
@@ -103,9 +102,38 @@ pub fn neighborhood_search_spatial_hashing_parallel(c: &mut Criterion) {
     group.finish();
 }
 
+pub fn neighborhood_search_spatial_hashing_seq_flat(c: &mut Criterion) {
+    let particle_positions: &Vec<Vector3<f32>> = &io::particles_from_file(PARTICLE_FILE).unwrap();
+    let particle_positions = particle_subset(particle_positions.as_slice());
+
+    let mut domain = Aabb3d::from_points(particle_positions);
+    domain.grow_uniformly(COMPACT_SUPPORT_RADIUS as f32);
+
+    let mut group = c.benchmark_group("neighborhood_search");
+    group.sample_size(100);
+    group.warm_up_time(Duration::from_secs(3));
+    group.measurement_time(Duration::from_secs(10));
+
+    let mut neighborhood_lists = FlatNeighborhoodList::default();
+    group.bench_function("neighborhood_search_spatial_hashing_seq_flat", move |b| {
+        b.iter(|| {
+            neighborhood_search::neighborhood_search_spatial_hashing_flat_filtered::<i32, f32>(
+                &domain,
+                particle_positions,
+                COMPACT_SUPPORT_RADIUS as f32,
+                &mut neighborhood_lists,
+                |_| true,
+            );
+        })
+    });
+
+    group.finish();
+}
+
 criterion_group!(
     bench_neighborhood,
     neighborhood_search_naive,
-    neighborhood_search_spatial_hashing,
+    neighborhood_search_spatial_hashing_seq,
     neighborhood_search_spatial_hashing_parallel,
+    neighborhood_search_spatial_hashing_seq_flat,
 );
